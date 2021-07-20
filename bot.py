613f21c0c672ff37fc9002b5dd26275374e6767f
Ranjeet Singh <techonerd1.0@gmail.com>
2021-07-20 15:32:00 +0530

    trying new API

--- bot.py
+++ bot.py
@@ -11,8 +11,6 @@ def bot():
     resp = MessagingResponse()
     msg = resp.message()
     responded = False
-    
-    # getting random quotes from quotable API
     if 'quote' in incoming_msg:
         # return a quote
         r = requests.get('https://api.quotable.io/random')
@@ -21,19 +19,40 @@ def bot():
             quote = f'{data["content"]} ({data["author"]})'
         else:
             quote = 'I could not retrieve a quote at this time, sorry.'
+        # quote = 'I love you baby <3 <3'
         msg.body(quote)
         responded = True
         
-    # getting random cat image
+    if 'slaps' in incoming_msg:
+        url = "https://markmscott-slapbot-v1.p.rapidapi.com/Twister"
+
+        headers = {
+            'x-rapidapi-key': "c5a8b25371mshfb8a44effa986d1p1a4abdjsn1541d6075333",
+            'x-rapidapi-host': "markmscott-slapbot-v1.p.rapidapi.com"
+            }
+
+        response = requests.request("GET", url, headers=headers)
+
+        print(response.text)
     if 'cat' in incoming_msg:
         # return a cat pic
         msg.media('https://cataas.com/cat')
         responded = True
+    if 'norris' or 'chunk' in incoming_msg:
+        r = requests.get('https://api.chucknorris.io/jokes/random')
+        if r.status_code ==200:
+            data = r.json()
+            quote = data["value"]
+        else:
+            quote = 'Ma bad. Got shot, bullet is in critical condition.'
+        # quote = 'I love you baby <3 <3'
+        msg.body(quote)
+        responded = True
 
-    # hinting users about possible methods
     if not responded:
-        msg.body('I only know about famous quotes and cats, sorry!')
+        msg.body('I only know about famous quotes, cats and chunk or norris or norris chunk')
     return str(resp)
+
 
 if __name__ == '__main__':
     app.run()
